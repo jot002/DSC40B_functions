b1dc83e83fa5c6a6dd36c7735ed3874e7cd0ecdb
jot002 <81881824+jot002@users.noreply.github.com>
2022-02-21 23:43:06 -0800

    Update biggest_descendent.py

--- biggest_descendent.py
+++ biggest_descendent.py
@@ -1,8 +1,8 @@
 import dsc40graph
-def biggest_descendant(graph, root, value, answer={}):
+def biggest_descendent(graph, root, value, answer={}):
     answer[root] = value[root]
     for node in graph.neighbors(root):
-        biggest_descendant(graph, node, value, answer)
+        biggest_descendent(graph, node, value, answer)
         if answer[root] < answer[node]:
             answer[root] = answer[node]
     return answer
