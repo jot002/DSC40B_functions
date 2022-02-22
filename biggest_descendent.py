ba1e50180633ac849ff0e41fcf89241b58901095
jot002 <81881824+jot002@users.noreply.github.com>
2022-02-21 23:39:32 -0800

    Create biggest_descendent.py

---
+++ biggest_descendent.py
@@ -0,0 +1,7 @@
+def biggest_descendant(graph, root, value, answer={}):
+    answer[root] = value[root]
+    for node in graph.neighbors(root):
+        biggest_descendant(graph, node, value, answer)
+        if answer[root] < answer[node]:
+            answer[root] = answer[node]
+    return answer
